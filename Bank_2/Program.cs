2cd6596cdc288786183ae0f628bbf5d8226072e3
Nastya <136957636+Mellaris@users.noreply.github.com>
2023-11-14 16:12:06 +0300

    Add files via upload

---
+++ Bank_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vib;
+            int number = 0;
+            List<bank> account_numbers = new List<bank>();
+            bank pol = new bank();
+            bank pol2 = new bank();
+            account_numbers.Add(pol);
+            account_numbers.Add(pol2);
+            Console.WriteLine("Перед началом работы необходимо выьрать счет (сейчас он пустой)");
+            Console.WriteLine("Нажмитие ниже: 1 или 2");
+            int account = Convert.ToInt32(Console.ReadLine()) - 1;
+            do
+            {
+                Console.WriteLine("Что вы хотели бы сделать?");
+                Console.WriteLine("0. Создать счет\n1. Показать инф о счете;\n2. Положить на счет;\n3. Снять со счета;\n4. Взять всю сумму;\n5. Перенести сумму с одного счета на другой.;\n6. Сменить счет. ");
+                vib = Convert.ToInt32(Console.ReadLine());
+                switch (vib)
+                {
+                    case 0: { account_numbers[account].Vibor(vib, account, account_numbers, number); break; }
+                    case 1: { account_numbers[account].Vibor(vib, account, account_numbers, number); break; }
+                    case 2: { account_numbers[account].Vibor(vib, account, account_numbers, number); break; }
+                    case 3: { account_numbers[account].Vibor(vib, account, account_numbers, number); break; }
+                    case 4: { account_numbers[account].Vibor(vib, account, account_numbers, number); break; }
+                    case 5:
+                        {
+                            Console.WriteLine("Введите номер счета, на который хотите перенести деньги(1 или 2)");
+                            number = Convert.ToInt32(Console.ReadLine()) - 1;
+                            account_numbers[account].Vibor(vib, account, account_numbers, number);
+                            break;
+                        }
+                    case 6:
+                        {
+                            if (account == 0)
+                            {
+                                account = 1;
+                            }
+                            else account = 0;
+                            break;
+                        }
+                }
+                Console.ReadKey();
+                Console.Clear();
+            } while (vib < 7);
+        }
+    }
+}
---
+++ Bank_2/bank.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Bank_2
+{
+    internal class bank
+    {
+        private int account_number;
+        private string name;
+        private float sum_account;
+        private float dob;
+        private float umen;
+        private void otk()
+        {
+            Console.Write("Введите номер своего счета: ");
+            account_number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите свое ФИО: ");
+            name = Console.ReadLine();
+            Console.Write("Введите сумму, которую хотели бы положить на счет: ");
+            sum_account = float.Parse(Console.ReadLine());
+        }
+        private void Out()
+        {
+            Console.WriteLine("Информация о вашем счете: ");
+            Console.WriteLine($"Номер счета: {account_number}");
+            Console.WriteLine($"ФИО: {name}");
+            Console.WriteLine($"Сумма на счету: {sum_account}");
+        }
+        private void Dob()
+        {
+            Console.Write("Введите сумму, которую хотели бы добавить на ваш счет: ");
+            dob = float.Parse(Console.ReadLine());
+            sum_account = sum_account + dob;
+            Console.WriteLine($"Сумма на счету: {sum_account}");
+        }
+        private void Umen()
+        {
+            Console.Write("Введите сумму, которую хотите снять со счета: ");
+            umen = float.Parse(Console.ReadLine());
+            if (umen > sum_account)
+            {
+                Console.WriteLine("У вас недостаточно средств");
+            }
+            else
+            {
+                sum_account = sum_account - umen;
+                Console.WriteLine($"Сумма на вашем счету: {sum_account}");
+            }
+        }
+        private void Obnul()
+        {
+            Console.WriteLine($"Снятие всей суммы: {sum_account}");
+            sum_account = sum_account - sum_account;
+        }
+        private void Perenos(int account, List<bank> account_numbers, int number)
+        {
+            if (account == number)
+            {
+                Console.WriteLine("Вы не можете переводить деньги со своего счета на свой");
+            }
+            else
+            {
+                Console.Write("Введите сумму, которую хотели бы добавить на счет: ");
+                dob = float.Parse(Console.ReadLine());
+                foreach (bank perevod in account_numbers)
+                {
+                    if (dob < sum_account)
+                    {
+                        perevod.sum_account = perevod.sum_account + dob;
+                        sum_account = sum_account - dob;
+                    }
+                }
+                Console.WriteLine($"Сумма на счету {name}: {sum_account}");
+            }
+        }
+        public void Vibor(int vib, int account, List<bank> account_numbers, int number)
+        {
+            switch (vib)
+            {
+                case 0: { otk(); break; }
+                case 1: { Out(); break; }
+                case 2: { Dob(); break; }
+                case 3: { Umen(); break; }
+                case 4: { Obnul(); break; }
+                case 5: { Perenos(account, account_numbers, number); ; break; }
+            }
+        }
+    }
+}

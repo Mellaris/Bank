f0b7d6a26a2c969f3ca250ac0feed6aa370e5543
Nastya <136957636+Mellaris@users.noreply.github.com>
2023-11-14 12:49:37 +0300

    Add files via upload

--- Bank/Bank/Class1.cs
+++ Bank/Bank/Class1.cs
@@ -8,42 +8,38 @@ namespace Bank
 {
     internal class Bank_Account
     {
-        private int account_number;
-        private string name;
-        private float sum_account;
+        public static int account_number;
+        public string name;
+        public float sum_account;
         private float dob;
         private float umen;
-        private int account_number_2;
-        private string name_2;
-        private float sum_account_2;
-        private int vibor;
+        public static int n;
         public void otk()
         {
-            Console.Write("Введите номер своего счета: ");
-            account_number = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите номер своего счета: ");
+            //account_number = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите свое ФИО: ");
             name = Console.ReadLine();
             Console.Write("Введите сумму, которую хотели бы положить на счет: ");
             sum_account = float.Parse(Console.ReadLine());
-            Vibor_2();
+            
         }
-        private void Out()
+        public void Out()
         {
             Console.WriteLine("Информация о вашем счете: ");
             Console.WriteLine($"Номер счета: {account_number}");
             Console.WriteLine($"ФИО: {name}");
             Console.WriteLine($"Сумма на счету: {sum_account}");
-            Vibor_2();
+            
         }
-        private void Dob()
+        public void Dob()
         {
             Console.Write("Введите сумму, которую хотели бы добавить на ваш счет: ");
             dob = float.Parse(Console.ReadLine());
             sum_account = sum_account + dob;
             Console.WriteLine($"Сумма на счету: {sum_account}");
-            Vibor_2();
         }
-        private void Umen() 
+        public void Umen() 
         {
             Console.Write("Введите сумму, которую хотите снять со счета: ");
             umen = float.Parse(Console.ReadLine());
@@ -56,62 +52,18 @@ namespace Bank
                 sum_account = sum_account - umen;
                 Console.WriteLine($"Сумма на вашем счету: {sum_account}");
             }
-            Vibor_2();
         }
-        private void Obnul()
+        public void Obnul()
         {
             Console.WriteLine($"Снятие всей суммы: {sum_account}");
             sum_account = sum_account - sum_account;
-            Vibor_2();
         }
-        private void Perenos()
+        public void Perenos()
         {
             Console.Write("Введите сумму, которую хотели бы добавить на счет: ");
             dob = float.Parse(Console.ReadLine());
             sum_account = sum_account + dob;
             Console.WriteLine($"Сумма на счету {name}: {sum_account}");
-            sum_account = sum_account - sum_account;
-            Vibor_2();
-        }
-        public void Vibor()
-        {
-            Console.WriteLine("Что вы хотели бы сделать?");
-            Console.WriteLine("0. Создать счет\n1. Показать инф о счете;\n2. Положить на счет;\n3. Снять со счета;\n4. Взять всю сумму;\n5. Перенести сумму с одного счета на другой. ");
-            int vib = Convert.ToInt32(Console.ReadLine());
-            if (vib == 0)
-            {
-                otk();
-            }
-            if (vib == 1)
-            {
-                Out();
-            }
-            if (vib == 2)
-            {
-                Dob();
-            }
-            if(vib == 3)
-            {
-                Umen();
-            }
-            if (vib == 4)
-            {
-                Obnul();
-            }
-            if (vib == 5)
-            {
-                Perenos();
-            }
-        }
-        private void Vibor_2()
-        {
-            Console.WriteLine("Чтобы вернуться назад, нажмите 9");
-            int vibor_2 = Convert.ToInt32(Console.ReadLine());
-            if (vibor_2 == 9)
-            {
-                Console.Clear();
-                Vibor();
-            }
         }
     }
 }
--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -10,16 +10,71 @@ namespace Bank
 {
     internal class Program
     {
+        public static int vib;
+        public static void In()
+        {
+            Console.Write("Введите номер своего счета: ");
+            Bank_Account.account_number = Convert.ToInt32(Console.ReadLine());
+            if (vib == 5)
+            {
+                Console.WriteLine("Введите номер счета, на который хотите перенести деньги");
+                Bank_Account.n = Convert.ToInt32(Console.ReadLine());
+            }
+        }
         static void Main(string[] args)
         {
-            List <Bank_Account> account_number = new List <Bank_Account> ();
+            List<Bank_Account> account_numbers = new List<Bank_Account>();
             Bank_Account pol = new Bank_Account();
             Bank_Account pol2 = new Bank_Account();
-            pol.Vibor();
-
-            account_number.Add(pol);
-            account_number.Add(pol2);
-
+            account_numbers.Add(pol);
+            account_numbers.Add(pol2);
+            do
+            {
+                Console.WriteLine("Что вы хотели бы сделать?");
+                Console.WriteLine("0. Создать счет\n1. Показать инф о счете;\n2. Положить на счет;\n3. Снять со счета;\n4. Взять всю сумму;\n5. Перенести сумму с одного счета на другой. ");
+                vib = Convert.ToInt32(Console.ReadLine());
+                switch (vib)
+                {
+                    case 0:
+                        {
+                            Program.In();
+                            account_numbers[Bank_Account.account_number].otk();
+                        }
+                        break;
+                    case 1:
+                        {
+                            Program.In();
+                            account_numbers[Bank_Account.account_number].Out();
+                        }
+                        break;
+                    case 2:
+                        {
+                            Program.In();
+                            account_numbers[Bank_Account.account_number].Dob();
+                        }
+                        break;
+                    case 3:
+                        {
+                            Program.In();
+                            account_numbers[Bank_Account.account_number].Umen();
+                        }
+                        break;
+                    case 4:
+                        {
+                            Program.In();
+                            account_numbers[Bank_Account.account_number].Obnul();
+                        }
+                        break;
+                    case 5:
+                        {
+                            Program.In();
+                            account_numbers[Bank_Account.account_number].Perenos();
+                        }
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+            } while (vib < 6);
         }
     }
 }
